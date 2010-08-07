19b90300a5c0a8716552c7047aaa76e4fa772ef2
Christopher Peplin <peplin@bueda.com>
2010-08-07 15:57:53 -0400

    Parse XML into more friendly class attributes.

--- hoppy/api.py
+++ hoppy/api.py
@@ -1,20 +1,20 @@
-from xml.dom.minidom import parseString
 from restkit import Resource
+from xml.dom.minidom import parseString
 
-account = None
-auth_token = None
-api_key = None
+from hoppy.util.xmldict import xml_to_dict
+from hoppy import account, auth_token
 
-class HoptoadError(Exception): pass
-
+class HoptoadApiError(Exception):
+    pass
 
 class HoptoadResource(Resource):
-    def __init__(self, use_ssl=False):
+    def __init__(self, use_ssl=False, data=None):
         self.auth_token = auth_token
         self.account = account
         self.host = self.base_uri(use_ssl)
         super(HoptoadResource, self).__init__(self.host, follow_redirect=True)
-        self.check_configuration()
+        if data:
+            self.from_xml(data)
 
     def base_uri(self, use_ssl=False):
         base = 'http://%s.hoptoadapp.com' % self.account
@@ -23,65 +23,20 @@ class HoptoadResource(Resource):
 
     def check_configuration(self):
         if not self.auth_token:
-            raise HoptoadError('API Token cannot be blank')
+            raise HoptoadApiError('auth token cannot be blank')
         if not self.account:
-            raise HoptoadError('Account cannot be blank')
+            raise HoptoadApiError('account cannot be blank')
 
     def request(self, *args, **kwargs):
+        self.check_configuration()
         response = super(HoptoadResource, self).request(
                 auth_token=self.auth_token, *args, **kwargs)
-        return parseString(response.body_string())
-
-
-class Error(HoptoadResource):
-    def find(self, error_id):
-        return self.get(self._error_path(error_id))
-
-    @staticmethod
-    def _error_path(error_id):
-        return 'errors/%d.xml' % error_id
-
-
-class Notice(HoptoadResource):
-    def find(self, notice_id, error_id):
-        return self.get(self._notice_path(notice_id, error_id))
-    
-    @staticmethod
-    def _notice_path(notice_id, error_id):
-        return 'errors/%(error_id)d/notices/%(notice_id)d.xml' % locals()
-
-
-class Deploy(Resource):
-    def __init__(self, use_ssl=False):
-        self.api_key = api_key
-        self.host = self.base_uri(use_ssl)
-        super(Deploy, self).__init__(self.host, follow_redirect=True)
-        self.check_configuration()
-
-    def check_configuration(self):
-        if not self.api_key:
-            raise HoptoadError('API Key cannot be blank')
-
-    def request(self, *args, **kwargs):
-        response = super(Deploy, self).request(
-                api_key=self.api_key, *args, **kwargs)
         return response.body_string()
 
-    def base_uri(self, use_ssl=False):
-        base = 'http://hoptoadapp.com/deploys.txt'
-        base = base.replace('http://', 'https://') if use_ssl else base
-        return base
+    def from_xml(self, data):
+        root = parseString(data)
+        self.from_dict(xml_to_dict(root))
 
-    def deploy(self, env, **kwargs):
-        """ Optional parameters accepted by Hoptoad are:
-        scm_revision
-        scm_repository
-        local_username
-        """
-
-        params = {}
-        params['deploy[rails_env]'] = env
-        for key, value in kwargs:
-            params['deploy[%s]' % key] = value
-
-        return self.post(**params)
+    def from_dict(self, data):
+        for key, value in data.iteritems():
+            setattr(self, key, value)
---
+++ hoppy/error.py
@@ -0,0 +1,13 @@
+from hoppy.api import HoptoadResource
+
+class HoptoadError(HoptoadResource):
+    def find(self, error_id):
+        return self.get(self._error_path(error_id))
+
+    @staticmethod
+    def _error_path(error_id):
+        return 'errors/%d.xml' % error_id
+
+    def request(self, *args, **kwargs):
+        response = super(HoptoadError, self).request(*args, **kwargs)
+        return HoptoadError(response)
